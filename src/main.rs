f5754a70be048a4ff325b857b5d7132fbf454faf
Marcelo Diop-Gonzalez <marcelo827@gmail.com>
2023-03-03 23:06:51 -0500

    move args

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,22 @@ mod apply_chunks;
 mod epoch_info;
 
 #[derive(clap::Parser)]
+struct RunCmd {
+    #[clap(long)]
+    neard_path: PathBuf,
+    #[clap(long)]
+    home_dir: PathBuf,
+}
+
+#[derive(clap::Parser)]
 enum SubCmd {
-    Run,
+    Run(RunCmd),
     PopulateEpochs,
     // TODO: TestParallelism(TestParallelismCmd),
 }
 
 #[derive(clap::Parser)]
 struct Cmd {
-    #[clap(long)]
-    neard_path: PathBuf,
-    #[clap(long)]
-    home_dir: PathBuf,
     #[clap(subcommand)]
     subcommand: SubCmd,
 }
@@ -35,13 +39,14 @@ async fn main() -> anyhow::Result<()> {
 
     let cmd: Cmd = clap::Parser::parse();
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url =
+        std::env::var("DATABASE_URL").context("error reading DATABASE_URL environment variable")?;
     let db = PgPool::connect(&database_url)
         .await
         .with_context(|| format!("Error connecting to {}", database_url))?;
 
     match cmd.subcommand {
-        SubCmd::Run => apply_chunks::apply_chunks(&db, &cmd.neard_path, &cmd.home_dir).await,
+        SubCmd::Run(cmd) => apply_chunks::apply_chunks(&db, &cmd.neard_path, &cmd.home_dir).await,
         SubCmd::PopulateEpochs => {
             epoch_info::populate(&db, "https://archival-rpc.mainnet.near.org", 85372640).await
         }
